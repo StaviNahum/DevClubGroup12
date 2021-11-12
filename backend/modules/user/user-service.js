c02e99d1edf24aa5a4f5c9a9efe412575638e66d
Stav <stavi1498@gmail.com>
2021-11-12 13:08:15 +0200

    add signin func

--- backend/modules/user/user-service.js
+++ backend/modules/user/user-service.js
@@ -5,3 +5,8 @@ const gUsers = require('../../db/user.json')
 function _addUserToDB() {
     fs.writeFileSync('data/car.json', JSON.stringify(gUsers, null, 2))
 }
+
+function _getUser(username){
+    const user = gUsers.find(u => u.id === username)
+    return user;
+}
