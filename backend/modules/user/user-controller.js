cd808b0d7a646fa512d665e57038d82ecdcd5eb2
Stav <stavi1498@gmail.com>
2021-11-12 13:49:20 +0200

    Add signin func

--- backend/modules/user/user-controller.js
+++ backend/modules/user/user-controller.js
@@ -3,19 +3,25 @@ const { addUser } = require("./user-service");
 gUsers = [];
 
 async function signin(req, res) {
+    try {
+        console.log(req.body);
+        const mess = await signin(req.body.username, req.body.password)
 
+        res.status(201).send(mess)
+
+    }
+    catch (err) {
+        res.status(500).send(err)
+    }
 }
 
 async function signup(req, res) {
-<<<<<<< HEAD
     const { v4: uuidv4 } = require('uuid');
     console.log(uuidv4());
 }
 
 async function signout(req, res)
-{
-    
-=======
+{    
     try {
         console.log(req.body);
         const user = await addUser(req.body)
@@ -29,7 +35,6 @@ async function signout(req, res)
 async function signout(req, res) {
 
 
->>>>>>> e124aeded60f766abffec570dfc50e36b59e3a5f
 }
 
 module.exports ={
