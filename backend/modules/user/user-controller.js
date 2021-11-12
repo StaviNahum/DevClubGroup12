e124aeded60f766abffec570dfc50e36b59e3a5f
[dudi samarel] <[dudisamarel@gmail.com]>
2021-11-12 13:07:50 +0200

    signup

--- backend/modules/user/user-controller.js
+++ backend/modules/user/user-controller.js
@@ -1,13 +1,24 @@
+const { addUser } = require("./user-service");
+
+gUsers = [];
+
 async function signin(req, res) {
+
 }
 
 async function signup(req, res) {
-    const { v4: uuidv4 } = require('uuid');
-    console.log(uuidv4());
-// asdasd
+    try {
+        console.log(req.body);
+        const user = await addUser(req.body)
+        res.send(user)
+    }
+    catch (err) {
+        res.status(500).send(err)
+    }
 }
 
 async function signout(req, res) {
+
 
 }
 
--- backend/modules/user/user-service.js
+++ backend/modules/user/user-service.js
@@ -1,7 +1,21 @@
 const fs = require('fs')
+const { v4: uuidv4 } = require('uuid');
 const gUsers = require('../../db/user.json')
 
+const addUser = ({ firstname, lastname, username, password }) => {
+    const _id = uuidv4()
+    if (!firstname || !lastname || !username || !password) return Promise.reject('All fields are required')
+    const user = {
+        _id, firstname, lastname, username, password
+    }
+    _addUserToDB(user);
+    return Promise.resolve(user)
+}
 
 function _addUserToDB() {
-    fs.writeFileSync('data/car.json', JSON.stringify(gUsers, null, 2))
+    fs.writeFileSync('../../db/user.json', JSON.stringify(gUsers, null, 2))
+}
+
+module.exports = {
+    addUser
 }
