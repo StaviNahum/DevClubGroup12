e72b376a3d9460470b09fae985799a892c6a3e18
Stav <stavi1498@gmail.com>
2021-11-14 01:04:36 +0200

    Update sign in func

--- backend/modules/user/user-controller.js
+++ backend/modules/user/user-controller.js
@@ -1,15 +1,16 @@
-const { addUser } = require("./user-service");
+const { addUser, auth } = require("./user-service");
 
 
 async function signin(req, res) {
     try {
         console.log(req.body);
-        const mess = await signin(req.body.username, req.body.password)
-
+        const mess = await auth(req.body.username, req.body.password)
+        console.log(mess)
         res.status(201).send(mess)
 
     }
     catch (err) {
+        console.log(err)
         res.status(500).send(err)
     }
 }
